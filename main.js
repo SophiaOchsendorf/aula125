664f99973d6ab372c77d5d87d9ece2b728507d06
SophiaOchsendorf <105024506+SophiaOchsendorf@users.noreply.github.com>
2022-11-28 10:13:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console,log('PoseNet Is Initialized!');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.X;
+        noseY = results[0].pose.nose.Y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWristX.X;
+        rightWristX = results[0].pose.rightWristX.X;
+        difference = floor( leftWristX - rightWristX);
+        
+        console.log("leftWristX = " + leftWristX  + " rightWristX = " + rightWristX + " difference = " + difference);
+    }
+}
+function draw() {
+    background
+    ('#969A97');
+
+    document.getElementById("square_side").innerHTML = "Altura e Largura serão = " + difference +"px";
+    fill('#f90093');
+    stroke('#f90093');
+    square(noseX, noseY, difference);
+}
